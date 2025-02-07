d7bfbd7d8060cf32608905a62ed8ac1885f9ae5e
Yiming Sun <33106916+YimingSun60@users.noreply.github.com>
2025-02-07 18:56:13 +0900

    socket 接收

--- sockettest.py
+++ sockettest.py
@@ -19,6 +19,20 @@ async def handler(websocket):
         connected_devices.remove(websocket)
         print(f"Remaining Devices: {len(connected_devices)}")
 
+async def send_message(message):
+
+    while True:
+        if connected_devices:
+            # 遍历所有已连接的客户端发送消息
+            for ws in connected_devices.copy():
+                try:
+                    await ws.send(message)
+                    print(f"已发送给 {ws.remote_address}: {message}")
+                except websockets.exceptions.ConnectionClosed:
+                    print(f"发送失败，设备已断开: {ws.remote_address}")
+        else:
+            print("当前没有连接的设备。")
+
 async def main():
     server = await websockets.serve(handler, "0.0.0.0", 12345)
     print("WebSocket server running on ws://0.0.0.0:12345")
