486e59ccb691d8f5adaaac04897ad9749d1b36a9
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2025-03-04 02:37:07 +0900

    feat: add sort option for loading

--- client/src/components/note-manager.ts
+++ client/src/components/note-manager.ts
@@ -1,13 +1,16 @@
 export type NoteData = {
   id: string;
   content: string;
-  date: string;
+  date: number;
 };
 
-const loadLocalNotes = (): NoteData[] => {
+const loadLocalNotes = (sortByDate: boolean): NoteData[] => {
   const localNotes: NoteData[] = Object.keys(localStorage)
     .filter((key) => key.startsWith("note-"))
     .map((key) => JSON.parse(localStorage.getItem(key) as string));
+  if (sortByDate) {
+    localNotes.sort((a, b) => b.date - a.date);
+  }
   return localNotes;
 };
 
@@ -15,19 +18,17 @@ const loadLocalNotes = (): NoteData[] => {
  * Clean the out of date notes if there are more than 10 notes
  */
 const cleanOutdatedNotes = () => {
-  const localNotes: NoteData[] = loadLocalNotes();
+  const localNotes: NoteData[] = loadLocalNotes(true);
   if (localNotes.length > 10) {
-    const sortedNotes: NoteData[] = localNotes.sort(
-      (a, b) => parseInt(b.date) - parseInt(a.date),
-    );
-    sortedNotes.slice(10).forEach((note) => {
+    localNotes.slice(10).forEach((note) => {
+      console.log("Note Deleted with Id: ", note.id);
       localStorage.removeItem("note-" + note.id);
     });
   }
 };
 
 const loadMostRecentNote = (): NoteData => {
-  const localNotes: NoteData[] = loadLocalNotes();
+  const localNotes: NoteData[] = loadLocalNotes(false);
   const mostRecentNote: NoteData = localNotes.reduce((prev, curr) => {
     return prev.date >= curr.date ? prev : curr;
   });
